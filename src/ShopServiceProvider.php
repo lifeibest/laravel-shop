bc19194d5576780ecd2cc67c8ef801b84f5c38fb
lifei <123178784@qq.com>
2018-04-07 10:05:19 +0800

    init

---
+++ src/ShopServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace Lifeibest\LaravelShop;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopServiceProvider extends ServiceProvider
+{
+
+    /**
+     * @var array
+     */
+    protected $commands = [
+        //'Lifeibest\LaravelShop\Console\TaskCommand',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+
+    }
+
+}
